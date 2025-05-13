f06aa340066089aaa67cbc2e5aba90f738e936e6
Любовь Хуснуллина <lyuba140296@gmail.com>
2025-05-14 00:39:12 +0500

    hash

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-import { HashRouterRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import "./styles/index.css";
 
 const rootView = document.getElementById("root");
